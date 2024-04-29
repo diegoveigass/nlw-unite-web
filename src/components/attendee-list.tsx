c349f066707e6ace19ba4aa769c6706d3863e7b8
Diego Veiga <diego.veiga@xmobots.com.br>
2024-04-29 16:32:15 -0300

    first class

---
+++ src/components/attendee-list.tsx
@@ -0,0 +1,130 @@
+import {
+  Search,
+  MoreHorizontal,
+  ChevronsLeft,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsRight,
+} from 'lucide-react'
+
+export function AttendeeList() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-2 items-center">
+        <h1 className="text-2xl font-bold">Participantes</h1>
+        <div className="px-3 py-1.5 border border-white/10 rounded-lg placeholder:text-gray-500 w-72 flex items-center gap-3">
+          <Search className="size-4 text-emerald-300" />
+          <input
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm ring-0"
+            placeholder="Buscar participante..."
+          />
+        </div>
+      </div>
+
+      <div className=" border border-white/10 rounded-lg">
+        <table className="w-full">
+          <thead>
+            <tr className="border-b border-white/10">
+              <th
+                style={{ width: 48 }}
+                className="py-3 px-4 text-sm font-semibold text-left"
+              >
+                <input
+                  type="checkbox"
+                  className="size-4 bg-black/20 rounded border border-white/10 checked:bg-orange-400"
+                />
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Código
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Participante
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Data de inscrição
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Data de check-in
+              </th>
+              <th
+                style={{ width: 64 }}
+                className="py-3 px-4 text-sm font-semibold text-left"
+              ></th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.from({ length: 10 }).map((_, index) => {
+              return (
+                <tr
+                  key={index}
+                  className="border-b border-white/10 hover:bg-white/5"
+                >
+                  <td className='className="py-3 px-4 text-sm text-zinc-300'>
+                    <input
+                      type="checkbox"
+                      className="size-4 bg-black/20 rounded border border-white/10 checked:bg-orange-400"
+                    />
+                  </td>
+                  <td className='className="py-3 px-4 text-sm text-zinc-300'>
+                    123412
+                  </td>
+                  <td className='className="py-3 px-4 text-sm text-zinc-300'>
+                    <div className="flex flex-col gap-1">
+                      <span className="font-semibold text-white">
+                        Diego Veiga Santos
+                      </span>
+                      <span>sdiegoveiga@gmail.com</span>
+                    </div>
+                  </td>
+                  <td className='className="py-3 px-4 text-sm '>
+                    7 dias atrás
+                  </td>
+                  <td className='className="py-3 px-4 text-sm text-zinc-300'>
+                    5 dias atrás
+                  </td>
+                  <td className='className="py-3 px-4 text-sm text-zinc-300'>
+                    <button className="bg-black/20 border border-white/10 rounded-md p-1.5">
+                      <MoreHorizontal className="size-4 " />
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td
+                className="py-3 px-4 text-sm font-semibold text-left"
+                colSpan={3}
+              >
+                Mostrando 10 de 228 itens
+              </td>
+              <td
+                className="py-3 px-4 text-sm font-semibold text-right"
+                colSpan={3}
+              >
+                <div className="inline-flex items-center gap-8">
+                  <span>Página 1 de 23</span>
+                  <div className="flex gap-1.5">
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <ChevronsLeft className="size-4 " />
+                    </button>
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <ChevronLeft className="size-4 " />
+                    </button>
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <ChevronRight className="size-4 " />
+                    </button>
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <ChevronsRight className="size-4 " />
+                    </button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  )
+}
